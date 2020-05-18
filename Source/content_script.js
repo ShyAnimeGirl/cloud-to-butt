82ddfe64c0c6ac5dea49eb49089d09c1bd7c8f1a
ShyAnimeGirl <honse246@gmail.com>
2020-05-18 18:59:27 -0400

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,13 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bCorona\b/g, "Coronu");
+	v = v.replace(/\bcorona\b/g, "coronu");
+	v = v.replace(/\bcovid-19\b/g, "chinaaids-666");
+	v = v.replace(/\bCovid-19\b/g, "gay-911");
+	v = v.replace(/\bCOVID-19\b/g, "6G");
+	v = v.replace(/\bCOVID\b/g, "CORONU");
+	v = v.replace(/\bcovid\b/g, "coronu");
 	
 	textNode.nodeValue = v;
 }
